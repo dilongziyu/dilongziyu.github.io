185e9df14bfd839bea821dd37874b5bcbf7aee47
dilongziyu <502329474@qq.com>
2018-04-03 17:33:30 +0800

    firstcommit

---
+++ demo/1/js/hdr_mn_frnt.js
@@ -0,0 +1,27 @@
+if(screen.width>800)
+{
+
+//瑕佽緭鍑虹殑"html浠ｇ爜"
+
+document.write('<div class="The_total hdr_bck">');
+document.write('<div class="hdr_con">');
+document.write('<div>');
+document.write('<div id="divLogin" class="lgn_a"></div>');
+document.write('<div id="divLoginSuccess"></div>');
+document.write('<div id="ExitCookies" class="lgn_a lgn_n"><a href="javascript:void(0);" onclick="$.cookie(&#39;userName&#39;,null,{ path: &#39;/&#39; ,domain: &#39;niceloo.com&#39; });$.cookie(&#39;Pwd&#39;,null,{path:&#39;/&#39;,domain: &#39;niceloo.com&#39; });$.cookie(&#39;UserId&#39;,null,{path: &#39;/&#39;,domain: &#39;niceloo.com&#39; });$.cookie(&#39;RealName&#39;,null,{path:&#39;/&#39;,domain: &#39;niceloo.com&#39; });window.location.reload();">退出</a></div>');
+document.write('</div>');
+document.write('<ul>');
+document.write('<li class="hdr_con_l_n"><a href="http://st.niceloo.com/KesionExtend/Audition/" target="_blank" class="hdr_con_a1">试听</a></li>');
+document.write('<li><a href="http://www.niceloo.com/zhuanti/zhibo/index.html" target="_blank" class="hdr_con_a2">直播</a></li>');
+document.write('<li><a href="http://www.niceloo.com/zhuanti/2015yltk/index.html" target="_blank" class="hdr_con_a3">题库</a></li>');
+document.write('<li><a href="http://www.niceloo.com/zhuanti/2014ydkt/" target="_blank" class="hdr_con_a4">客户端</a></li>');
+document.write('<li><a href="http://www.niceloo.com/zhuanti/wxpt/index.html" target="_blank" class="hdr_con_a5">微信</a></li>');
+document.write('</ul>');
+document.write('</div>');
+document.write('<div class="dv_mn" id="nav">');
+document.write('<div id="hdr_mn">');
+document.write('<h1><a href="http://www.niceloo.com/" target="_blank"><img src="images/logo.png" alt=""></a><span id="demo"></span>');
+document.write('</h1>');
+document.write('<div>');
+
+}
